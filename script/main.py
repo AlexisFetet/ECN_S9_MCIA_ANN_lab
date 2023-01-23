ccf0972f9df336ceafdb2655970a7d45e6703fb8
Alexis Fetet <fetet.alexis@gmail.com>
2023-01-23 13:35:05 +0100

    apply gaussian to each weight independantly

--- script/main.py
+++ script/main.py
@@ -1,4 +1,9 @@
+"""
+This file contains a python script to construct a neural network to work on MNIST dataset
+"""
+
 import os
+
 import numpy as np
 
 TRAINING_IMAGES = os.path.abspath("../data/training/train-images-idx3-ubyte")
@@ -6,10 +11,17 @@ TRAINING_LABELS = os.path.abspath("../data/training/train-labels-idx1-ubyte")
 TEST_IMAGES = os.path.abspath("../data/test/t10k-images-idx3-ubyte")
 TEST_LABELS = os.path.abspath("../data/test/t10k-labels-idx1-ubyte")
 
+
 class Layer():
+    """
+    represents a layer of the network
+    """
 
     def __init__(self, connections_in_count, connections_out_count, func, d_func, alpha) -> None:
-        self.weight = np.ones((connections_out_count, connections_in_count)) * np.random.normal(0, 1/np.sqrt(connections_in_count))
+        self.weight = np.array(list(map(
+            lambda x: x * np.random.normal(0,
+                                           1/np.sqrt(connections_in_count)),
+            np.ones((connections_out_count, connections_in_count)))))
         self.bias = np.zeros((connections_out_count, 1))
         self.activation = func
         self.d_activation = d_func
@@ -20,57 +32,71 @@ class Layer():
         self.last_input = np.zeros((connections_in_count, 1))
 
     def forward(self, input_vector):
+        """
+        computes output of the layer
+        """
         self.last_input = input_vector
         self.last_z = np.dot(self.weight, input_vector) + self.bias
         self.last_a = self.activation(self.last_z)
         return self.last_a
 
     def backward(self, delta, weight):
-        self.last_delta = np.dot(self.d_activation(self.last_z) * weight, delta)
+        """
+        computes error based on previous layer error and weight
+        """
+        self.last_delta = np.dot(
+            self.d_activation(self.last_z) * weight, delta)
         return self.last_delta
-    
+
     def update(self):
+        """
+        update bias and weights
+        """
         self.weight = self.weight - self.alpha * self.last_delta * self.last_input
         self.bias = self.bias - self.alpha * self.last_delta
 
 
-def activation_function(x) -> np.ndarray:
-    return 1/(1+np.exp(x))
+def activation_function(input_vector) -> np.ndarray:
+    """
+    activation function
+    """
+    return 1/(1+np.exp(input_vector))
 
-def d_activation_function(x) -> np.ndarray:
-    temp = activation_function(x)
+
+def d_activation_function(input_vector) -> np.ndarray:
+    """
+    derivative of the activation function
+    """
+    temp = activation_function(input_vector)
     return temp*(1-temp)
 
 
-
-
 if __name__ == '__main__':
-    
 
     ALPHA = 1.1  # pas d'apprentissage
 
     w1 = np.array([[0.9, 1.1],
-                [1, 1]])
+                   [1, 1]])
 
     w2 = np.array([[1, 1],
-                [1, 1]])
+                   [1, 1]])
 
     b1 = np.array([[-1],
-                [-2]])
+                   [-2]])
 
     b2 = np.array([[-1],
-                [-1]])
+                   [-1]])
 
     a0 = np.array([[1],
-                [1]])
+                   [1]])
 
     y = np.array([[0],
-                [1]])
+                  [1]])
 
     a2 = np.array([[0],
-                [0]])
+                   [0]])
 
-    while(abs(sum(a2-y)) > 1e-5):
+    while abs(sum(a2-y)) > 1e-5:
         # calcul de la sortie
         z1 = np.dot(w1, a0) + b1
         a1 = activation_function(z1)
